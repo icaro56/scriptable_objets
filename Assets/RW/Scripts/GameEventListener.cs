332b96bd53541b3c47263f5f2bc4134574316e74
icaro56 <icarogoulart@yahoo.com.br>
2019-08-20 17:01:30 -0300

    Scriptable Tutorial raywenderlich done

---
+++ Assets/RW/Scripts/GameEventListener.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameEvent> gameEvents = new List<GameEvent>();
+
+    [SerializeField]
+    private UnityEvent response;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < gameEvents.Count; ++i)
+        {
+            gameEvents[i].RegisterListener(this);
+        }
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < gameEvents.Count; ++i)
+        {
+            gameEvents[i].UnregisterListener(this);
+        }
+    }
+
+    public void OnEventRaised()
+    {
+        response.Invoke();
+    }
+}
---
+++ Assets/RW/Scripts/SoundPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public AudioSource efxSource;
+
+    //Used to play single sound clips.
+    public void Play()
+    {
+        //Play the clip.
+        efxSource.Play();
+    }
+}
